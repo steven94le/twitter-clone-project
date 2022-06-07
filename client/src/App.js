5ceeb7d94c19a0b30dbb58e5b8fd0e59e5147fd5
Steven <le.steven@hotmail.com>
2022-06-07 12:21:20 -0400

    base layout of textarea on homefeed

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Sidebar from "./components/Sidebar";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import styled from "styled-components";
 import GlobalStyles from "./components/GlobalStyles";
+import { COLORS } from "./constants";
 
 import HomeFeed from "./components/HomeFeed";
 import Notifications from "./components/Notifications";
@@ -11,17 +12,18 @@ import TweetDetails from "./components/TweetDetails";
 import Profile from "./components/Profile";
 
 import { CurrentUserContext } from "./components/CurrentUserContext";
+// import { HomeFeedContext } from "./components/HomeFeedContext";
 
 const App = () => {
-  const { currentUser, status } = useContext(CurrentUserContext);
-  console.log("currentUser", currentUser);
-  console.log("status", status);
+  const { status } = useContext(CurrentUserContext);
+  // console.log("currentUser", currentUser);
+  // console.log("status", status);
 
   return (
     <>
       {status === "loading" ? (
         //update loading message/screen
-        <p>Loading...</p>
+        <StyledLoadPara>Loading...</StyledLoadPara>
       ) : (
         <>
           <GlobalStyles />
@@ -30,6 +32,15 @@ const App = () => {
               <Sidebar />
               <Switch>
                 <Route exact path="/">
+                  <div>Home</div>
+                  <div>
+                    <div>User Avatar</div>
+                    <StyledTextArea placeholder="What's Happening" />
+                  </div>
+                  <TextAreaActionArea>
+                    <div>Counter</div>
+                    <TextAreaButton>Meow</TextAreaButton>
+                  </TextAreaActionArea>
                   <HomeFeed />
                 </Route>
                 <Route exact path="/notifications">
@@ -55,6 +66,34 @@ const App = () => {
 
 const StyledDiv = styled.div`
   margin-left: 300px;
+`;
+
+const StyledLoadPara = styled.p`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+const StyledTextArea = styled.textarea`
+  border: 1px solid grey;
+  outline: none;
+  resize: none;
+  width: 550px;
+  height: 100px;
+`;
+
+const TextAreaActionArea = styled.div`
+  display: flex;
+  justify-content: right;
+  width: 550px;
+`;
+
+const TextAreaButton = styled.button`
+  background-color: ${COLORS.primary};
+  color: white;
+  border-radius: 15px;
+  font-size: 16px;
 `;
 
 export default App;
